818439dbad81ab835ff239268e97abcd39f0ceff
Nikita Nyatin <yesnik@yandex.ru>
2020-05-02 11:00:12 +0500

    Add PublishedManager

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -17,6 +22,13 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+            self.publish.month, self.publish.day, self.slug])
 
     class Meta:
         ordering = ('-publish',)
