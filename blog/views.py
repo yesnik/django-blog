ba6b753850e8b8695664b436af7d917531159a8d
Nikita Nyatin <yesnik@yandex.ru>
2020-05-03 12:56:16 +0500

    Add comments to posts

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -16,7 +16,26 @@ class PostListView(ListView):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                                    publish__month=month, publish__day=day)
-    return render(request,'blog/post/detail.html',{'post': post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    comment_form = CommentForm()
+    
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+        else:
+            comment_form = CommentForm()
+
+    return render(request,'blog/post/detail.html', {
+        'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form
+    })
 
 
 def post_share(request, post_id):
