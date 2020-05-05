a91591846fbd050c31b21d227965b1d70cdf99a4
Nikita Nyatin <yesnik@yandex.ru>
2020-05-05 12:05:27 +0500

    Add similar posts

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
@@ -14,9 +15,13 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
     def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        context['tag'] = self.get_tag(self.get_slug())
+        
+        slug = self.get_slug()
+
+        if slug:
+            context['tag'] = self.get_tag(slug)
+        
         return context
 
     def get_queryset(self):
@@ -57,11 +62,18 @@ def post_detail(request, year, month, day, post):
         else:
             comment_form = CommentForm()
 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags','-publish')[:4]
+
     return render(request,'blog/post/detail.html', {
         'post': post,
         'comments': comments,
         'new_comment': new_comment,
-        'comment_form': comment_form
+        'comment_form': comment_form,
+        'similar_posts': similar_posts
     })
 
 
